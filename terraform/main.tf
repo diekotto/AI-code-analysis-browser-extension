075fee770f87bc6b0966d3d4c09f03b73255fcc0
Diego Maroto <diego.diemg@gmail.com>
2024-11-15 09:57:33 +0100

    Removed permissions related to bedrock due to wtf service feeling.

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,31 +5,7 @@ output "function_url" {
   value = module.api_lambda.function_url
 }
 
-resource "aws_iam_policy" "bedrock_policy" {
-  name        = "${var.prefix}-api-bedrock-policy"
-  description = "IAM policy for Lambda to invoke Bedrock"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "bedrock:InvokeModel",
-          "bedrock:InvokeModelWithResponseStream"
-        ]
-        Resource = [
-          "arn:aws:bedrock:eu-central-1::foundation-model/anthropic.claude-3-5-sonnet-20240620-v1*",
-          "arn:aws:bedrock:eu-central-1::foundation-model/anthropic.claude-3-5-sonnet-20241022-v2*",
-        ]
-      }
-    ]
-  })
-}
 module "api_lambda" {
-  providers = {
-    aws = aws.bedrock
-  }
   source       = "./modules/lambda"
   description  = "API of Code Analysis Assistant"
   environment  = var.environment
@@ -39,9 +15,6 @@ module "api_lambda" {
   prefix       = var.prefix
   timeout      = "10"
   function_url = true
-  attach_policies = {
-    BEDROCK = aws_iam_policy.bedrock_policy.arn
-  }
 }
 
 
