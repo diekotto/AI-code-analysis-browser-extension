57930d089e17129edb8694c7dc3e77ce3c5cbdb7
Diego Maroto <diego.diemg@gmail.com>
2024-11-15 09:58:24 +0100

    Added some tests to the sidebar section

--- extension/src/background.js
+++ extension/src/background.js
@@ -1,3 +1,5 @@
 chrome.runtime.onInstalled.addListener(() => {
   console.log('Extension installed');
 });
+
+chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true }).catch((error) => console.error(error));
